0b0f4725c689658a66a5b1c26ca4af0b6358ca8c
Carlos Eduardo <carloseduardo1357910@gmail.com>
2024-06-23 16:44:10 -0300

    Trabalho ainda em estágio inicial.

---
+++ jogoDaForca.py
@@ -0,0 +1,31 @@
+import json
+import requests #importando a biblioteca capaz de realizar requisições.
+address = arquivo.json
+content = requests.get(address)
+data = content.json()
+len(data)
+type(data)
+import random
+secret_value = random.choice(data)
+tips = secret_value["dica"]
+name = secret_value["palavra"]
+print(f"Está palavra possui {len(name)} caracteres e a seguinte descrição: ")
+print(tips)
+while True:
+  response = input("Qual a tecnologia utilizada?")
+  if response == name:
+    print("Resposta correta!")
+    break
+  else:
+    print("Resposta incorreta, tente novamente!")
+
+
+
+
+
+
+
+
+
+
+    # "https://raw.githubusercontent.com/guilhermeonrails/api-imersao-ia/main/words.json"
